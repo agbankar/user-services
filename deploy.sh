d67a54839545e08ceac589fa538604efe5a4d866
AS008BA <ashish.bankar@pb.com>
2021-03-08 14:17:38 +0530

    rm -rf user-service

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 rm -rf user-service
 git clone https://github.com/agbankar/user-service.git
-cd user-service
-docker build -t shahpriti919/user-service .
+docker build -t shahpriti919/user-service ./user-service
 docker login -u shahpriti919 -p $urekha12
 docker push shahpriti919/user-service
 kubectl delete deployment user-service-deployment
 kubectl delete svc user-service-svc
-kubectl apply -f user-deployment.yml
-kubectl apply -f  user-service.yml
+kubectl apply -f ./user-service/user-deployment.yml
+kubectl apply -f  ./user-serviceuser-service.yml
 kubectl get deployments
 kubectl get svc
