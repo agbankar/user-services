7340a1fcf4a123293a4bca883104714dc7b02cfa
AS008BA <ashish.bankar@pb.com>
2021-03-08 14:54:37 +0530

    rm -rf user-service

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ rm -rf user-service
 git clone https://github.com/agbankar/user-service.git
 cd user-service && mvn clean install
 docker build -t shahpriti919/user-service ./user-service
-docker login -u shahpriti919 -p $urekha12
+docker login -u="shahpriti919" -p="$urekha12"
 docker push shahpriti919/user-service
 kubectl delete deployment user-deployment
 kubectl delete svc user-service-svc
-kubectl apply -f ./user-service/user-deployment.yml
-kubectl apply -f  ./user-service/user-service.yml
+kubectl apply -f user-deployment.yml
+kubectl apply -f user-service.yml
 kubectl get deployments
 kubectl get svc
