7360be18da53947ac3fd008d43dd2edbc136e80b
AS008BA <ashish.bankar@pb.com>
2021-03-08 13:04:21 +0530

    adding script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker build -t shahpriti919/user-service .
+docker login -u shahpriti919 -p $urekha12
+docker push shahpriti919/user-service
+kubectl delete deployment user-service-deployment
+kubectl delete svc user-service-svc
+kubectl apply -f user-deployment.yml
+kubectl apply -f  user-service.yml
+kubectl get deployments
+kubectl get svc
+
